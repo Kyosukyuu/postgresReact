7a390709ee68aed86a8f2c68672cff5038816b78
Kyosukyuu <82899136+Kyosukyuu@users.noreply.github.com>
2021-06-22 01:43:58 -0400

    basic code to connect to postgres db and test it

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 require("dotenv").config();
 
 const app = require("express")();
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
+
+const { Sequelize } = require("sequelize");
+
+const { DB_NAME, DB_USER, DB_PW } = process.env;
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PW, {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+require("./testDB")(sequelize);
 
 app.listen(PORT, () => {
   console.log("Listening on port", PORT);
---
+++ backend/testDB.js
@@ -0,0 +1,10 @@
+const testDB = async (db) => {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+module.exports = testDB;
